81135e9e5721d7d40b705fe3ef8583c425021055
Lukas Eugen Marsoner Steinkasserer <36311765+lukasmarsoner@users.noreply.github.com>
2021-08-29 00:12:14 +0200

    Ziemlich dreckiger Hack um mit Avataren umzugehen
    Logik um %-Angaben in SVGs von https://avatars.dicebear.com/ umzugehen. Dieser Hack funktioniert nur für den Fall den wir brauchen

--- lib/src/utilities/numbers.dart
+++ lib/src/utilities/numbers.dart
@@ -3,14 +3,21 @@
 /// Passing `null` will return `null`.
 ///
 /// Will strip off a `px` prefix.
-double? parseDouble(String? maybeDouble, {bool tryParse = false}) {
+double? parseDouble(String? maybeDouble,
+    {bool tryParse = false, String xy = ''}) {
   assert(tryParse != null); // ignore: unnecessary_null_comparison
   if (maybeDouble == null) {
     return null;
   }
-  maybeDouble = maybeDouble.replaceFirst('px', '').trim();
+  maybeDouble = maybeDouble.trim().replaceFirst('px', '').trim();
+  // Ziemlich dreckiger Hack nur für mich
+  if (maybeDouble.contains('%') && xy != null) {
+    double _percentage = double.tryParse(maybeDouble.replaceAll('%', '')) ?? 0;
+    maybeDouble = (_percentage / 100 - (xy == 'x' ? 0 : 0.25)).toString();
+  }
   if (tryParse) {
     return double.tryParse(maybeDouble);
   }
   return double.parse(maybeDouble);
 }
+
