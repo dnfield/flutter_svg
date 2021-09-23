5eb1d2d0051c01076388de007973cadd933da5ad
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2021-09-23 16:56:16 +0200

    test: update SvgTheme tests

--- test/theme_test.dart
+++ test/theme_test.dart
@@ -7,16 +7,6 @@ import 'package:test/test.dart';
 void main() {
   group('SvgTheme', () {
     group('constructor', () {
-      test('sets colorFilter', () {
-        const ColorFilter colorFilter =
-            ColorFilter.mode(Color(0xFF00FF00), BlendMode.color);
-
-        expect(
-          SvgTheme(colorFilter: colorFilter).colorFilter,
-          equals(colorFilter),
-        );
-      });
-
       test('sets currentColor', () {
         const Color currentColor = Color(0xFFB0E3BE);
 
@@ -30,12 +20,10 @@ void main() {
     test('supports value equality', () {
       expect(
         SvgTheme(
-          colorFilter: ColorFilter.mode(Color(0xFF00FF00), BlendMode.darken),
           currentColor: Color(0xFF6F2173),
         ),
         equals(
           SvgTheme(
-            colorFilter: ColorFilter.mode(Color(0xFF00FF00), BlendMode.darken),
             currentColor: Color(0xFF6F2173),
           ),
         ),
